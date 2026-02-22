5adc255045c1177639f14a08f89876b546ad7c4b
Thomas Krol <thomaskrol22@protonmail.com>
2026-02-22 13:57:46 +0100

    flesh out basic index.ts

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log("Hello via Bun!");
+import express from "express";
+import type { Express, Request, Response } from "express";
+import axios from "axios";
+
+const app: Express = express();
+const PORT: number = 3000;
+const API_URL: string = "https://api.spoonacular.com";
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", async (_, res: Response) => {
+  res.render("index.ejs");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
